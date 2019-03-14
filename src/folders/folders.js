afff99ba9653198c5072d63ee8b6aac3a4b01668
Jafar Naqvi <naqvi.jafar91@gmail.com>
2019-03-14 22:00:10 +0530

    WIP

--- src/folders/folders.js
+++ src/folders/folders.js
@@ -2,40 +2,49 @@ import React, { Component } from 'react';
 import './folders.css';
 class Folders extends Component {
 
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            folders: [
+                {
+                    id: 1,
+                    name: 'Inbox',
+                    remaining_tasks: 13
+                },
+                {
+                    id: 2,
+                    name: 'Grocery',
+                    remaining_tasks: 1234
+                },
+                {
+                    id: 3,
+                    name: 'Work',
+                    remaining_tasks: 2
+                }
+            ]
+        };
+    }
+
+    enableFolderNameEditor() {
+        // Hide that span and make input for that folder visible
+    }
+
     render() {
-        const editIconStyle = {
-            fontSize: '0.75em'
-          };
+        const folders = this.state.folders.map((folder) => {
+            return <li>
+                <div>
+                    <span>{folder.name}</span>
+                    <i class="fa fa-pencil rename-folder-icon" aria-hidden="true"></i>
+                    <span class="remaining-tasks">{folder.remaining_tasks}</span>
+                </div>
+            </li>
+        });
+
         return (
-        <div id="folders">
-            <span id="add-folder">+</span>
-            <ul>
-                <li>
-                    <div>
-                        <span>Inbox</span>
-                        <i class="fa fa-pencil rename-folder-icon" aria-hidden="true"
-                        style={editIconStyle}></i>
-                        <span class="remaining-tasks">13</span>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>Work</span>
-                        <i class="fa fa-pencil rename-folder-icon" aria-hidden="true"
-                        style={editIconStyle}></i>
-                        <span class="remaining-tasks">103</span>
-                    </div>
-                </li>
-                <li>
-                    <div>
-                        <span>Grocery</span>
-                        <i class="fa fa-pencil rename-folder-icon" aria-hidden="true"
-                        style={editIconStyle}></i>
-                        <span class="remaining-tasks">3</span>
-                    </div>
-                </li>
-            </ul>
-        </div>
+            <div id="folders">
+                <span id="add-folder">+</span>
+                <ul>{folders}</ul>
+            </div>
         );
     }
 }
