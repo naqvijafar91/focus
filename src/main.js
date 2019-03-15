596f417fa8bd83d5aa1889daef61538dc9f947d1
Jafar Naqvi <naqvi.jafar91@gmail.com>
2019-03-15 22:20:20 +0530

    Added login page template

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import FolderItem from './folders/folderItem';
+import Folders from './folders/folders';
+import Tasks from './tasks/tasks';
+import Helmet from 'react-helmet';
+
+class Main extends Component {
+    constructor(props,context) {
+        super(props,context);
+    }
+}
