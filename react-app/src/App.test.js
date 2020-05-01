5f7c82df112bd0c5d58fcf8aba1804ca4bf8fdf2
Jafar <naqvi.jafar91@gmail.com>
2020-05-01 14:56:11 +0530

    updated tests

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import UserStore from './LoginPage/userStore';
 import App from './App';
-
+UserStore.saveUser({token:"sample-test"});
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
